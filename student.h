426458b6dae2329c63a5e69a39e8449b61730067
Sebastián Vallejo Roldán <sebastianvallejoroldan@gmail.com>
2021-08-29 00:45:23 -0500

    Update student.h

--- student.h
+++ student.h
@@ -8,6 +8,12 @@ struct Obj_t{
     int cedula;
 };
 
+struct _courseInfo_t{
+    char name[32];
+    int credits;
+    float grade;
+};
+
 void feature1(FILE *fin, FILE *fout);
 void feature2(FILE *fin, FILE *fout);
 void feature3(FILE *fin, FILE *fout);
@@ -15,7 +21,7 @@ void feature4(FILE *fin, int **parr, int *length, char **op);
 void feature5(FILE *fout, int *parr, int length, char *op);
 void feature6(FILE *fin, struct Obj_t *pobj);
 void feature7(FILE *fout, struct Obj_t *pobj);
-void feature8(FILE *fin, struct Obj_t **pobj,int *length);
-void feature9(FILE *fout, struct Obj_t *pobj,int length);
+void feature8(FILE *fin, struct _courseInfo_t **pobj,int *length);
+void feature9(FILE *fout, struct _courseInfo_t *pobj,int length);
 
 #endif
